73a3db52cc6cabb001b86d404557278a027988a3
Ahmed Saleem Shaikh <98047091+AhmedShaykhETH@users.noreply.github.com>
2022-04-10 01:46:47 +0500

    It's Time To Push !

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
-console.log('Web Animation API');
+var elem = document.querySelector('#box');
+
+elem.animate([
+    {transform: 'translate(0 , 0)'},
+    {transform: 'translate(600px , 0)'},
+    {transform: 'translate(0 , 0)'}
+],{
+    duration: 1500,
+    iterations: Infinity
+})
+
+var elem = document.querySelector('#box2');
+
+elem.animate([
+    {transform: 'translate(0 , 0)'},
+    {transform: 'translate(400px , 400px)'},
+    {transform: 'translate(0 , 0)'}
+],{
+    duration: 1500,
+    iterations: Infinity
+})
