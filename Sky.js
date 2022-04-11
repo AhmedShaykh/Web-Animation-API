216fbc28be3717344fb7a868727d48ddd8a6b288
Ahmed Saleem Shaikh <98047091+AhmedShaykhETH@users.noreply.github.com>
2022-04-11 23:02:24 +0500

    It's Time To Push !

--- Sky.js
+++ Sky.js
@@ -1,1 +1,30 @@
-var elem
+var elem = document.getElementById('clouds');
+
+var animatedeeElem = elem.animate([
+    { transform: 'translate(800px , 0)' },
+    { transform: 'translate(50px , 0)' },
+], {
+    duration: 10000,
+    iterations: Infinity
+})
+
+var elem = document.getElementById('birds');
+
+var animatedeeElem = elem.animate([
+    { transform: 'translate(0 , 0)' },
+    { transform: 'translate(900px , 0)' },
+], {
+    duration: 15000,
+    iterations: Infinity,
+    playbackRate: -4
+})
+
+var elem = document.getElementById('plane');
+
+var animatedeeElem = elem.animate([
+    { transform: 'translate(0 , 0)' },
+    { transform: 'translate(900px , 0)' },
+], {
+    duration: 3000,
+    iterations: Infinity
+})
